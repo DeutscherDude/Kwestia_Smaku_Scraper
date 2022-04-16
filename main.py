1dff5299d4701e512085b3c0e7e7f0ffb6f2f463
DeutscherDude <kolokius@gmail.com>
2022-04-16 23:15:43 +0200

    Entering Recipes works, iterating thorugh items in progress

--- main.py
+++ main.py
@@ -9,48 +9,57 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
+def scrape_data(content: webdriver, method: str) -> None:
+
+
+
+
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.maximize_window()
 driver.get('https://www.kwestiasmaku.com/')
-content = driver.page_source
 
-soup = BeautifulSoup(content, 'html.parser')
 recipes = []
+tags = []
 ingredients = []
 preparation = []
 food_imgs = []
 
+el_to_hover = driver.find_element(By.XPATH, "//div[@class='main-menu']//a[text()[contains(.,'Posiłki')]]")  
 
-el_to_hover = driver.find_element(By.XPATH, "//div[@class='main-menu']//a[text()[contains(.,'Posiłki')]]")
-
-# el_to_hover = driver.find_element(By.XPATH, "//div[@class='main-menu']//span[@class='icon icomoon icon-poradnia']")
 hover = ActionChains(driver)
-hover.move_to_element(el_to_hover).perform()
+hover.move_to_element(el_to_hover).click().perform()
 
-main_category = soup.find('a', text='Posiłki', href=True)
-# //div[@class="main-menu"]//ul[@class="depth_-1 menu"]//li[@class="has-children"]
+content = driver.page_source
+
+next_found = True
+
+while next_found:
+    for element in driver.find_elements(By.XPATH, "//div[@class='views-bootstrap-grid-plugin-style']//div[@class='col col-lg-3']//img[@class='img-responsive']"):
+
+        soup = BeautifulSoup(content, 'html.parser')
+        name = element.find('div', attrs={'itemprop': 'name'})
+
+        img = element.find('img', attrs={'class': 'img-responsive'})
+        recipes.append(name.text.strip())
+        food_imgs.append(img['src'])
+
+        link = driver.find_element(By.XPATH, "//div[@class='views-bootstrap-grid-plugin-style']//div[@class='col col-lg-3']//img[@class='img-responsive']").click()
+        hf_cont = driver.page_source
+        
+        rec_ing = soup.find('div', class_='group-skladniki')
+        rec_ing = rec_ing.get_text()
+        ingredients.append(rec_ing)
+        prep = soup('div', class_='group-przepis field-group-div')
+        preparation.append(prep)
+        
+        tag = soup.find('ul', class_='field field-name-field-przepisy field-type-taxonomy-term-reference field-label-hidden')
+        tag = tag.get_text()
+        tags.append(tag)
+        driver.back()
+        print(tags)
 
 
-# soup.find_all(href=re.compile("elsie"), id='link1')
-# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>]
-
-for element in soup.find_all('div', class_='col col-lg-3'):
-    name = element.find('div', attrs={'class': 'views-field-title'})
-
-    img = element.find('img', attrs={'class': 'img-responsive'})
-    recipes.append(name.text.strip())
-    food_imgs.append(img['src'])
-
-    link = driver.find_element(By.XPATH, "//div[@class='field field-name-field-zdjecie field-type-image field-label-hidden']").click()
-    hf_cont = driver.page_source
-    sub_soup = BeautifulSoup(hf_cont, 'html.parser')
-    rec_ing = sub_soup.find('div', class_='group-skladniki')
-    ingredients.append(rec_ing)
-    prep = sub_soup('div', class_='group-przepis field-group-div')
-    preparation.append(prep)
-    driver.back()
-
-df = pd.DataFrame({'recipes': recipes, 'ingredients': ingredients, 'preparation': preparation, 'food_imgs': food_imgs})
+df = pd.DataFrame({'tags': tags, 'recipes': recipes, 'ingredients': ingredients, 'preparation': preparation, 'food_imgs': food_imgs})
 df.to_csv('przepisy.csv', index=False, encoding='utf-8')
