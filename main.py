a92d70798fb50766cf9dc78b8c9e4e4edab72c8f
DeutscherDude <kolokius@gmail.com>
2022-04-17 16:57:36 +0200

    Introduced try except to handle Timeout exceptions

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from argparse import Action
-from json.tool import main
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
@@ -13,36 +10,42 @@ import pandas as pd
 
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.maximize_window()
-driver.get('https://www.kwestiasmaku.com/')
+driver.get('https://www.kwestiasmaku.com/przepisy/posilki')
 
 recipes = []
 tags = []
 ingredients = []
 preparation = []
 
-el_to_hover = driver.find_element(By.XPATH, "//div[@class='main-menu']//a[text()[contains(.,'Posiłki')]]")  
-
-hover = ActionChains(driver)
-hover.move_to_element(el_to_hover).click().perform()
-
 next_found = True
 elements = driver.find_elements(By.XPATH, "//div[@class='views-bootstrap-grid-plugin-style']//div[@class='col col-lg-3']//img[@class='img-responsive']")
 
+k = 1
+
 while next_found:
     for i in range(0, len(elements)):
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(elements[i])).click()
-        # elements[i].click()
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(elements[i])).click()
+            # elements[i].click()
+        except TimeoutException:
+            print('Element could not be found')
+            break
+        try:        
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='group-przepis field-group-div']")))
+        except TimeoutException:
+            print('Element could not be found')
+            break    
+            
         content = driver.page_source
         soup = BeautifulSoup(content, 'html.parser')
         name = soup.find('div', attrs={'itemprop': 'name'})
 
-        # To fix explicit waiting for an element to be located
-        
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "group-skladniki field-group-div")))
+        driver.implicitly_wait(1)
         rec_ing = soup.find('div', class_='group-skladniki field-group-div')
+        driver.implicitly_wait(1)
         rec_ing = rec_ing.get_text()
         ingredients.append(rec_ing)
         prep = soup('div', class_='group-przepis field-group-div')
@@ -52,10 +55,14 @@ while next_found:
         tag = tag.get_text()
         tags.append(tag)
 
+        driver.implicitly_wait(1)
         driver.back()
-        driver.implicitly_wait(3)
         
         elements = driver.find_elements(By.XPATH, "//div[@class='views-bootstrap-grid-plugin-style']//div[@class='col col-lg-3']//img[@class='img-responsive']")
+        
+        k = k + 1
+        print(f"Beep {i}")
+
     try:
         driver.find_element(By.XPATH, '//li[@class="next last"]').click()
         driver.implicitly_wait(1.5)
